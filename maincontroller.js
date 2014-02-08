d2fccb8cc7ad676035031d3bd9741641d72c0877
Gustavo Padovan <gustavo.padovan@collabora.co.uk>
2014-02-08 15:08:13 -0200

    fix indentation and use array length

--- maincontroller.js
+++ maincontroller.js
@@ -1,10 +1,13 @@
 app.controller("MainController", function($scope, $http){
-    $http.get('phrases.json')
+
+	var phrases;
+
+	$http.get('phrases.json')
 	.then(function(res){
-		$scope.phrases = res.data;
+		phrases = res.data;
 	});
 
-    $scope.fala_shehah = function() {
-		$scope.shit = $scope.phrases[Math.floor(Math.random()*6)].shit
-    }
+	$scope.fala_shehah = function() {
+		$scope.shit = phrases[Math.floor(Math.random()*phrases.length)].shit
+	}
 });
